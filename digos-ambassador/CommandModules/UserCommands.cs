ea172fbe9c89e7627279cc035e520796662ae194
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-11-03 18:04:52 +0100

    Implement basic user info display.

--- digos-ambassador/CommandModules/UserCommands.cs
+++ digos-ambassador/CommandModules/UserCommands.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -27,6 +28,8 @@ namespace DIGOS.Ambassador.CommandModules
 				// Add the user to the user database if they're not already in it
 				user = db.GetOrRegisterUser(this.Context.Message.Author);
 			}
+
+			await ShowUserInfoAsync(this.Context.Message.Author, user);
 		}
 
 		/// <summary>
@@ -42,6 +45,79 @@ namespace DIGOS.Ambassador.CommandModules
 			{
 				user = db.GetOrRegisterUser(discordUser);
 			}
+
+			await ShowUserInfoAsync(discordUser, user);
+		}
+
+		/// <summary>
+		/// Shows a nicely formatted info block about a user.
+		/// </summary>
+		/// <param name="discordUser">The Discord user.</param>
+		/// <param name="user">The stored information about the user.</param>
+		/// <returns>A task wrapping the command.</returns>
+		private async Task ShowUserInfoAsync(IUser discordUser, User user)
+		{
+			var eb = new EmbedBuilder();
+
+			eb.WithAuthor(this.Context.User);
+			eb.WithThumbnailUrl(discordUser.GetAvatarUrl());
+
+			switch (user.Class)
+			{
+				case UserClass.Other:
+				{
+					eb.WithColor(1.0f, 1.0f, 1.0f); // White
+					break;
+				}
+				case UserClass.DIGOSDronie:
+				{
+					eb.WithColor(Color.DarkOrange);
+					break;
+				}
+				case UserClass.DIGOSUnit:
+				{
+					eb.WithColor(Color.Purple);
+					break;
+				}
+				default:
+				{
+					throw new ArgumentOutOfRangeException();
+				}
+			}
+
+			eb.AddField("Name", discordUser.Username);
+			eb.AddField("Class", user.Class);
+
+			string timezoneValue;
+			if (user.Timezone == null)
+			{
+				timezoneValue = "No timezone set.";
+			}
+			else
+			{
+				timezoneValue = "UTC";
+				if (user.Timezone >= 0)
+				{
+					timezoneValue += "+";
+				}
+
+				timezoneValue += user.Timezone.Value;
+			}
+			eb.AddField("Timezone", timezoneValue);
+
+			string bioValue;
+			if (string.IsNullOrEmpty(user.Bio))
+			{
+				bioValue = "No bio set.";
+			}
+			else
+			{
+				bioValue = user.Bio;
+			}
+
+			eb.AddField("Bio", bioValue);
+
+			await this.Context.Channel.SendMessageAsync(string.Empty, false, eb);
 		}
 	}
 }
