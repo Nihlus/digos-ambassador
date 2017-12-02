1fe70a4685cfd321bd67cd0212b6f5ae752d45bf
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-12-02 17:19:48 +0100

    Add an implementation of Levenshtein distance.

---
+++ digos-ambassador/Modules/KinkCommands.cs
@@ -0,0 +1,62 @@
+﻿//
+//  DossierCommands.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System.Threading.Tasks;
+using Discord.Commands;
+
+#pragma warning disable SA1615 // Disable "Element return value should be documented" due to TPL tasks
+
+namespace DIGOS.Ambassador.Modules
+{
+	/// <summary>
+	/// Commands for viewing and configuring user kinks.
+	/// </summary>
+	[Group("kink")]
+	[Summary("Commands for viewing and configuring user kinks.")]
+	public class KinkCommands : ModuleBase<SocketCommandContext>
+	{
+		public async Task ShowKinkAsync()
+		{
+
+		}
+
+		public async Task ShowKinkOverlap()
+		{
+
+		}
+
+		public async Task ShowKinksByPreferenceAsync()
+		{
+
+		}
+
+		public async Task SetKinkPreferenceAsync()
+		{
+
+		}
+
+		public async Task RunKinkWizardAsync()
+		{
+
+		}
+	}
+}
---
+++ digos-ambassador/Utility/LevenshteinDistance.cs
@@ -0,0 +1,86 @@
+﻿//
+//  LevenshteinDistance.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+
+namespace DIGOS.Ambassador.Utility
+{
+	/// <summary>
+	/// Contains approximate string matching
+	/// </summary>
+	public static class LevenshteinDistance
+	{
+		/// <summary>
+		/// Compute the distance between two strings.
+		/// </summary>
+		/// <param name="s">The first string.</param>
+		/// <param name="t">The second string.</param>
+		/// <returns>The operation distance between the strings.</returns>
+		public static int Compute(string s, string t)
+		{
+			int n = s.Length;
+			int m = t.Length;
+			int[,] d = new int[n + 1, m + 1];
+
+			// Step 1
+			if (n == 0)
+			{
+				return m;
+			}
+
+			if (m == 0)
+			{
+				return n;
+			}
+
+			// Step 2
+			for (int i = 0; i <= n; d[i, 0] = i++)
+			{
+			}
+
+			for (int j = 0; j <= m; d[0, j] = j++)
+			{
+			}
+
+			// Step 3
+			for (int i = 1; i <= n; i++)
+			{
+				//Step 4
+				for (int j = 1; j <= m; j++)
+				{
+					// Step 5
+					int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+					// Step 6
+					d[i, j] = Math.Min
+					(
+						Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+						d[i - 1, j - 1] + cost
+					);
+				}
+			}
+
+			// Step 7
+			return d[n, m];
+		}
+	}
+}
