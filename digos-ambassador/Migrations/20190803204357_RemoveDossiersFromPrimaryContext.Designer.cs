7bdfcac3d1c94f00dd8139489a1e7b35d74d1299
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2019-08-03 22:48:37 +0200

    Remove the dossier table from the main context.

--- digos-ambassador/Migrations/20190803204357_RemoveDossiersFromPrimaryContext.Designer.cs
+++ digos-ambassador/Migrations/20190803204357_RemoveDossiersFromPrimaryContext.Designer.cs
@@ -8,14 +8,16 @@ using DIGOS.Ambassador.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DIGOS.Ambassador.Migrations
 {
     [DbContext(typeof(AmbyDatabaseContext))]
-    partial class GlobalInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20190803204357_RemoveDossiersFromPrimaryContext")]
+    partial class RemoveDossiersFromPrimaryContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
