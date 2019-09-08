beb51c3527dbcd27cdbfbf86066bacee57e28eff
Jarl Gullberg <jarl.gullberg@gmail.com>
2019-09-08 13:04:18 +0200

    Add migration.

--- Plugins/DIGOS.Ambassador.Plugins.Characters/Migrations/20190908110052_ImproveDataAnnotations.Designer.cs
+++ Plugins/DIGOS.Ambassador.Plugins.Characters/Migrations/20190908110052_ImproveDataAnnotations.Designer.cs
@@ -8,14 +8,16 @@ using DIGOS.Ambassador.Plugins.Characters.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DIGOS.Ambassador.Plugins.Characters.Migrations
 {
     [DbContext(typeof(CharactersDatabaseContext))]
-    partial class CharactersDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20190908110052_ImproveDataAnnotations")]
+    partial class ImproveDataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
