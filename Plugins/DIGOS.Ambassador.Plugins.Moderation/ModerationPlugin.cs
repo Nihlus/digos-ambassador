0cae3b0410e5142636a8a7265e91e78a6d69105b
Jarl Gullberg <jarl.gullberg@gmail.com>
2019-09-23 16:53:53 +0200

    Stub out the plugin.

---
+++ Plugins/DIGOS.Ambassador.Plugins.Moderation/ModerationPlugin.cs
@@ -0,0 +1,77 @@
+//
+//  ModerationPlugin.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Threading.Tasks;
+using DIGOS.Ambassador.Core.Database.Extensions;
+using DIGOS.Ambassador.Plugins.Abstractions;
+using DIGOS.Ambassador.Plugins.Abstractions.Attributes;
+using DIGOS.Ambassador.Plugins.Moderation;
+using DIGOS.Ambassador.Plugins.Moderation.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: AmbassadorPlugin(typeof(ModerationPlugin))]
+
+namespace DIGOS.Ambassador.Plugins.Moderation
+{
+    /// <summary>
+    /// Describes the moderation plugin.
+    /// </summary>
+    public class ModerationPlugin : PluginDescriptor, IMigratablePlugin
+    {
+        /// <inheritdoc />
+        public override string Name => "Moderation";
+
+        /// <inheritdoc />
+        public override string Description => "Provides simple moderation tools.";
+
+        /// <inheritdoc />
+        public override Task<bool> RegisterServicesAsync(IServiceCollection serviceCollection)
+        {
+            serviceCollection
+                .AddSchemaAwareDbContextPool<ModerationDatabaseContext>();
+
+            return Task.FromResult(true);
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> MigratePluginAsync(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<ModerationDatabaseContext>();
+
+            await context.Database.MigrateAsync();
+
+            return true;
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> IsDatabaseCreatedAsync(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<ModerationDatabaseContext>();
+            var appliedMigrations = await context.Database.GetAppliedMigrationsAsync();
+
+            return appliedMigrations.Any();
+        }
+    }
+}
