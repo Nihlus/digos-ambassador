aab272dd204a71b49cc5e796740d1f92ae6d24f0
Jarl Gullberg <jarl.gullberg@gmail.com>
2019-09-24 15:52:30 +0200

    Stub out command modules.

---
+++ Plugins/DIGOS.Ambassador.Plugins.Moderation/CommandModules/ModerationServerCommands.cs
@@ -0,0 +1,42 @@
+//
+//  ModerationServerCommands.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using Discord.Commands;
+using JetBrains.Annotations;
+
+#pragma warning disable SA1615 // Disable "Element return value should be documented" due to TPL tasks
+
+namespace DIGOS.Ambassador.Plugins.Moderation.CommandModules
+{
+    public partial class ModerationCommands
+    {
+        /// <summary>
+        /// Server-related commands, such as viewing or editing info about a specific server.
+        /// </summary>
+        [PublicAPI]
+        [Group("server")]
+        [Summary("Server-related commands, such as viewing or editing info about a specific server.")]
+        public partial class ModerationServerCommands : ModuleBase
+        {
+        }
+    }
+}
