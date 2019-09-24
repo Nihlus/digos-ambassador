f4f91569c6df5c1c85ecfc7843f2181a4d4bc177
Jarl Gullberg <jarl.gullberg@gmail.com>
2019-09-24 16:22:51 +0200

    Add commands for editing server-wide mod settings.

--- Plugins/DIGOS.Ambassador.Plugins.Moderation/CommandModules/ModerationServerSetCommands.cs
+++ Plugins/DIGOS.Ambassador.Plugins.Moderation/CommandModules/ModerationServerSetCommands.cs
@@ -20,8 +20,15 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
+using System.Threading.Tasks;
+using DIGOS.Ambassador.Discord.Feedback;
+using DIGOS.Ambassador.Plugins.Moderation.Permissions;
+using DIGOS.Ambassador.Plugins.Moderation.Services;
+using DIGOS.Ambassador.Plugins.Permissions.Preconditions;
+using Discord;
 using Discord.Commands;
 using JetBrains.Annotations;
+using PermissionTarget = DIGOS.Ambassador.Plugins.Permissions.Model.PermissionTarget;
 
 #pragma warning disable SA1615 // Disable "Element return value should be documented" due to TPL tasks
 
@@ -38,6 +45,83 @@ namespace DIGOS.Ambassador.Plugins.Moderation.CommandModules
             [Group("set")]
             public class ModerationServerSetCommands : ModuleBase
             {
+                [NotNull]
+                private readonly ModerationService _moderation;
+
+                [NotNull]
+                private readonly UserFeedbackService _feedback;
+
+                /// <summary>
+                /// Initializes a new instance of the <see cref="ModerationServerSetCommands"/> class.
+                /// </summary>
+                /// <param name="moderation">The moderation service.</param>
+                /// <param name="feedback">The feedback service.</param>
+                public ModerationServerSetCommands
+                (
+                    [NotNull] ModerationService moderation,
+                    [NotNull] UserFeedbackService feedback
+                )
+                {
+                    _moderation = moderation;
+                    _feedback = feedback;
+                }
+
+                /// <summary>
+                /// Sets the moderation log channel.
+                /// </summary>
+                /// <param name="channel">The channel.</param>
+                [Command("moderation-log-channel")]
+                [Summary("Sets the moderation log channel.")]
+                [RequirePermission(typeof(EditModerationServerSettings), PermissionTarget.Self)]
+                public async Task SetModerationLogChannel([NotNull] ITextChannel channel)
+                {
+                    var setChannel = await _moderation.SetModerationLogChannelAsync(this.Context.Guild, channel);
+                    if (!setChannel.IsSuccess)
+                    {
+                        await _feedback.SendErrorAsync(this.Context, setChannel.ErrorReason);
+                        return;
+                    }
+
+                    await _feedback.SendConfirmationAsync(this.Context, "Channel set.");
+                }
+
+                /// <summary>
+                /// Sets the event monitoring channel.
+                /// </summary>
+                /// <param name="channel">The channel.</param>
+                [Command("event-monitoring-channel")]
+                [Summary("Sets the event monitoring channel.")]
+                [RequirePermission(typeof(EditModerationServerSettings), PermissionTarget.Self)]
+                public async Task SetMonitoringChannel([NotNull] ITextChannel channel)
+                {
+                    var setChannel = await _moderation.SetMonitoringChannelAsync(this.Context.Guild, channel);
+                    if (!setChannel.IsSuccess)
+                    {
+                        await _feedback.SendErrorAsync(this.Context, setChannel.ErrorReason);
+                        return;
+                    }
+
+                    await _feedback.SendConfirmationAsync(this.Context, "Channel set.");
+                }
+
+                /// <summary>
+                /// Sets the warning threshold.
+                /// </summary>
+                /// <param name="threshold">The threshold.</param>
+                [Command("warning-threshold")]
+                [Summary("Sets the warning threshold.")]
+                [RequirePermission(typeof(EditModerationServerSettings), PermissionTarget.Self)]
+                public async Task SetWarningThreshold(int threshold)
+                {
+                    var setChannel = await _moderation.SetWarningThresholdAsync(this.Context.Guild, threshold);
+                    if (!setChannel.IsSuccess)
+                    {
+                        await _feedback.SendErrorAsync(this.Context, setChannel.ErrorReason);
+                        return;
+                    }
+
+                    await _feedback.SendConfirmationAsync(this.Context, "Threshold set.");
+                }
             }
         }
     }
---
+++ Plugins/DIGOS.Ambassador.Plugins.Moderation/Permissions/EditModerationServerSettings.cs
@@ -0,0 +1,44 @@
+//
+//  EditModerationServerSettings.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using DIGOS.Ambassador.Plugins.Permissions;
+using JetBrains.Annotations;
+
+namespace DIGOS.Ambassador.Plugins.Moderation.Permissions
+{
+    /// <summary>
+    /// Represents a permission that allows a user to edit server-specific moderation settings.
+    /// </summary>
+    [PublicAPI]
+    public sealed class EditModerationServerSettings : Permission
+    {
+        /// <inheritdoc />
+        public override Guid UniqueIdentifier { get; } = new Guid("A41141B7-1010-43DA-9372-2866B1CB78F7");
+
+        /// <inheritdoc />
+        public override string FriendlyName => nameof(EditModerationServerSettings);
+
+        /// <inheritdoc />
+        public override string Description => "Allows you to edit server-specific moderation settings.";
+    }
+}
