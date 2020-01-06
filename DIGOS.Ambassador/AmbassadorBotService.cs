832506ad1b571d6d16c5357ff35f3ccf6bf7f347
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-01-06 19:10:41 +0100

    Move to using Remora.Discord.Hosted.

--- DIGOS.Ambassador/AmbassadorBotService.cs
+++ DIGOS.Ambassador/AmbassadorBotService.cs
@@ -21,39 +21,26 @@
 //
 
 using System;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using DIGOS.Ambassador.Core.Services;
-using DIGOS.Ambassador.Discord.Interactivity.Behaviours;
-using DIGOS.Ambassador.Plugins.Abstractions.Database;
-using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using JetBrains.Annotations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Remora.Behaviours;
 using Remora.Behaviours.Services;
+using Remora.Discord.Hosted;
 using Remora.Plugins.Services;
 using Remora.Results;
 
-#pragma warning disable SA1118 // Parameter spans multiple lines, big strings
-
-// ReSharper disable RedundantDefaultMemberInitializer - suppressions for indirectly initialized properties.
 namespace DIGOS.Ambassador
 {
     /// <summary>
     /// Main service for the bot itself. Handles high-level functionality.
     /// </summary>
-    public class AmbassadorBotService : HostedBotService<AmbassadorBotService>
+    public class AmbassadorBotService : HostedDiscordBotService<AmbassadorBotService>
     {
-        private readonly DiscordSocketClient _client;
-        private readonly BehaviourService _behaviours;
-        private readonly PluginService _pluginService;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="AmbassadorBotService"/> class.
         /// </summary>
@@ -78,192 +65,34 @@ namespace DIGOS.Ambassador
             IHostApplicationLifetime applicationLifetime,
             IServiceProvider services
         )
-            : base(hostConfiguration, hostEnvironment, log, applicationLifetime, services)
+            : base
+            (
+                discordClient,
+                pluginService,
+                behaviourService,
+                hostConfiguration,
+                hostEnvironment,
+                log,
+                applicationLifetime,
+                services
+            )
         {
-            _client = discordClient;
-            _behaviours = behaviourService;
-            _pluginService = pluginService;
-
-            _client.Log += OnDiscordLogEvent;
             commandService.Log += OnDiscordLogEvent;
         }
 
-        /// <inheritdoc />
-        public override async Task StartAsync(CancellationToken cancellationToken)
-        {
-            if (!await InitializePluginsAsync())
-            {
-                this.Log.LogError("Failed to initialize the available plugins.");
-
-                // Plugin failures means we won't continue
-                this.Lifetime.StopApplication();
-
-                return;
-            }
-
-            var loginResult = await LoginAsync();
-            if (!loginResult.IsSuccess)
-            {
-                this.Log.LogError(loginResult.Exception, loginResult.ErrorReason);
-
-                // Login failures means we won't continue
-                this.Lifetime.StopApplication();
-
-                return;
-            }
-
-            await _behaviours.AddBehaviourAsync<InteractivityBehaviour>(this.Services);
-            await _behaviours.AddBehaviourAsync<DelayedActionBehaviour>(this.Services);
-
-            await _client.StartAsync();
-            await _behaviours.StartBehavioursAsync();
-        }
-
-        /// <inheritdoc />
-        public override async Task StopAsync(CancellationToken cancellationToken)
-        {
-            this.Log.LogInformation("Stopping behaviours...");
-            await _behaviours.StopBehavioursAsync();
-
-            await _client.LogoutAsync();
-            await _client.StopAsync();
-        }
-
-        private async Task<bool> InitializePluginsAsync()
-        {
-            var plugins = _pluginService.LoadAvailablePlugins().ToList();
-
-            // Create plugin databases
-            foreach (var plugin in plugins)
-            {
-                if (!(plugin is IMigratablePlugin migratablePlugin))
-                {
-                    continue;
-                }
-
-                if (await migratablePlugin.IsDatabaseCreatedAsync(this.Services))
-                {
-                    continue;
-                }
-
-                if (await migratablePlugin.MigratePluginAsync(this.Services))
-                {
-                    continue;
-                }
-
-                this.Log.LogWarning
-                (
-                    $"The plugin \"{plugin.Name}\" (v{plugin.Version}) failed to create its database."
-                );
-
-                return false;
-            }
-
-            // Then, run migrations in reverse
-            foreach (var plugin in plugins.AsEnumerable().Reverse())
-            {
-                if (!(plugin is IMigratablePlugin migratablePlugin))
-                {
-                    continue;
-                }
-
-                if (await migratablePlugin.MigratePluginAsync(this.Services))
-                {
-                    continue;
-                }
-
-                this.Log.LogWarning
-                (
-                    $"The plugin \"{plugin.Name}\" (v{plugin.Version}) failed to migrate its database."
-                );
-
-                return false;
-            }
-
-            foreach (var plugin in plugins)
-            {
-                if (await plugin.InitializeAsync(this.Services))
-                {
-                    continue;
-                }
-
-                this.Log.LogWarning
-                (
-                    $"The plugin \"{plugin.Name}\" (v{plugin.Version}) failed to initialize."
-                );
-
-                return false;
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// Logs the ambassador into Discord.
-        /// </summary>
-        /// <returns>A task representing the login action.</returns>
-        private async Task<ModifyEntityResult> LoginAsync()
+        /// <inheritdoc/>
+        protected override async Task<RetrieveEntityResult<string>> GetTokenAsync()
         {
             var contentService = this.Services.GetRequiredService<ContentService>();
 
             var getTokenResult = await contentService.GetBotTokenAsync();
             if (!getTokenResult.IsSuccess)
             {
-                return ModifyEntityResult.FromError(getTokenResult);
+                return RetrieveEntityResult<string>.FromError(getTokenResult);
             }
 
             var token = getTokenResult.Entity.Trim();
-
-            await _client.LoginAsync(TokenType.Bot, token);
-
-            return ModifyEntityResult.FromSuccess();
-        }
-
-        /// <summary>
-        /// Saves log events from Discord using the configured method in log4net.
-        /// </summary>
-        /// <param name="arg">The log message from Discord.</param>
-        /// <returns>A completed task.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown if the log severity is not recognized.</exception>
-        [NotNull]
-        private Task OnDiscordLogEvent(LogMessage arg)
-        {
-            var content = $"Discord log event: {arg.Message}";
-            switch (arg.Severity)
-            {
-                case LogSeverity.Critical:
-                {
-                    this.Log.LogCritical(content, arg.Exception);
-                    break;
-                }
-                case LogSeverity.Error:
-                {
-                    this.Log.LogError(content, arg.Exception);
-                    break;
-                }
-                case LogSeverity.Warning:
-                {
-                    this.Log.LogWarning(content, arg.Exception);
-                    break;
-                }
-                case LogSeverity.Verbose:
-                case LogSeverity.Info:
-                {
-                    this.Log.LogInformation(content, arg.Exception);
-                    break;
-                }
-                case LogSeverity.Debug:
-                {
-                    this.Log.LogDebug(content, arg.Exception);
-                    break;
-                }
-                default:
-                {
-                    throw new ArgumentOutOfRangeException();
-                }
-            }
-
-            return Task.CompletedTask;
+            return token;
         }
     }
 }
