6937fc4f9bdaf6a0734bbcf0e802a62d7f833371
Jarl Gullberg <jarl.gullberg@gmail.com>
2021-03-20 21:54:21 +0100

    Suppress finalization.

--- Tests/DIGOS.Ambassador.Tests/TestBases/DatabaseProvidingTestBase.cs
+++ Tests/DIGOS.Ambassador.Tests/TestBases/DatabaseProvidingTestBase.cs
@@ -66,6 +66,7 @@ namespace DIGOS.Ambassador.Tests.TestBases
         /// <inheritdoc />
         public void Dispose()
         {
+            GC.SuppressFinalize(this);
             _connection.Dispose();
         }
     }
